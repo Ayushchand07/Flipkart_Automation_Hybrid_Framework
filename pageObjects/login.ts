e7b7c2cdc1a72cae0ab8d70fbd327aa439cd52df
Ayush Chand <aayushchand900@gmail.com>
2025-09-05 01:11:14 +0530

    Session stor

--- pageObjects/login.ts
+++ pageObjects/login.ts
@@ -4,9 +4,28 @@ export class LoginPage{
 
     readonly page: Page
     readonly userNameField: Locator
-    readonly passwordFied: Locator
-    readonly loginButton: Locator
+    readonly requestOtpButton: Locator
+    readonly loginIcon: Locator
     readonly signupButton: Locator
+    readonly text : Locator
+    readonly name: Locator
+    
+    constructor(page){
+        this.loginIcon = this.page.locator('.-dOa_b L_FVxe');
+        this.userNameField = this.page.locator('.r4vIwl BV+Dqf')
+        this.requestOtpButton = this.page.locator('.QqFHMw twnTnD _7Pd1Fp')
+        this.text = this.page.getByText('Please enter the OTP sent to')
+        this.name = this.page.getByText('Ayush')
+        
+    }
+
+    async login(){
+        await this.loginIcon.click()
+        await this.userNameField.fill('aayushchand900@gmail.com')
+        await this.requestOtpButton.click()
+        // await expect (this.text).toBeVisible()
+        // await expect (this.text).toBeVisible()
+    }
     
 
 
--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,3 +1,8 @@
 import {test} from 'playwright/test'
+import { LoginPage } from '../pageObjects/login'
 
+test('login', async({page})=>{
+    const loginpage = new LoginPage(page)
+    await loginpage.login()
+})
 
