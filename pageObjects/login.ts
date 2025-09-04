66cf025b0526cc334b7028efe3c467ce9634bae1
Ayush Chand <aayushchand900@gmail.com>
2025-09-04 22:30:28 +0530

    just some locators

--- pageObjects/login.ts
+++ pageObjects/login.ts
@@ -1,1 +1,13 @@
-asdfasd
+import {expect, Locator,Page } from 'playwright/test'
+
+export class LoginPage{
+
+    readonly page: Page
+    readonly userNameField: Locator
+    readonly passwordFied: Locator
+    readonly loginButton: Locator
+    readonly signupButton: Locator
+    
+
+
+}
